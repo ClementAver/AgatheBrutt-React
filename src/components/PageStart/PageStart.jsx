8e94b3678f737e094d19f609404a5e12f2ecba9f
ClementAver <clement.aver@yahoo.fr>
2023-07-25 13:01:40 +0200

    Adds 'PageStart' behavior.

--- src/components/PageStart/PageStart.jsx
+++ src/components/PageStart/PageStart.jsx
@@ -1,11 +1,21 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { HashLink } from "react-router-hash-link";
 
 export default function PageStart() {
+  const [className, setClassName] = useState("cInvisible");
+
+  const handleScroll = () => {
+    setClassName(window.pageYOffset > 100 ? "cVisible" : "cInvisible");
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
   return (
-    <Link
+    <HashLink
       id="cRetour"
-      class="cInvisible"
-      to="#haut"
-    ></Link>
+      smooth
+      className={className}
+      to="#top"
+    ></HashLink>
   );
 }
--- src/pages/projects/LuneBleue/LuneBleue.jsx
+++ src/pages/projects/LuneBleue/LuneBleue.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { projects } from "../../../data/data";
 import IntroProject from "../../../components/IntroProjects/IntroProject";
+import PageStart from "../../../components/PageStart/PageStart";
 
 export default function LuneBleue() {
   const [project, setProject] = useState(null);
@@ -18,13 +19,24 @@ export default function LuneBleue() {
   if (project !== null) {
     return (
       <>
-        <IntroProject
-          title={project.title}
-          summary={project.summary}
-          date={project.date}
-          tags={project.tags}
-          furtherInformation={furtherInformation}
-        />
+        <main className="projet_main">
+          <div className="contenu">
+            <IntroProject
+              title={project.title}
+              summary={project.summary}
+              date={project.date}
+              tags={project.tags}
+              furtherInformation={furtherInformation}
+            />
+
+            <div className="intro"></div>
+            <div className="intro"></div>
+            <div className="intro"></div>
+            <div className="intro"></div>
+            <div className="intro"></div>
+          </div>
+        </main>
+        <PageStart />
       </>
     );
   }
