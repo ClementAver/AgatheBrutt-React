b23efa90500e63307d705fa0da75c15b3259c349
ClementAver <clement.aver@yahoo.fr>
2023-07-27 07:41:25 +0200

    setup gh-pages - refactors app and uses hashrouter.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import { Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import IndexP from "./pages/Index/Index";
+import Project from "./pages/Project/Project";
+import LuneBleue from "./pages/projects/LuneBleue/LuneBleue";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <Outlet />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/index"
+          element={<IndexP />}
+        />
+        <Route
+          path="/about"
+          element={<About />}
+        />
+        <Route
+          path="/project"
+          element={<Project />}
+        />
+        <Route
+          path="/project/lune-bleue"
+          element={<LuneBleue />}
+        />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./utils/router";
-
+import { HashRouter as Router } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import App from "./";
 
-createRoot(document.getElementById("root")).render(<RouterProvider router={router} />);
+createRoot(document.getElementById("root")).render(
+  <Router>
+    <App />
+  </Router>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
