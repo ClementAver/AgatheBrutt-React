f98f6d951e37d4e52e2611a029a7d05d89ff1c5c
ClementAver <clement.aver@yahoo.fr>
2023-07-27 07:44:44 +0200

    fixes 'App' import.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter as Router } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
-import App from "./";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <Router>
